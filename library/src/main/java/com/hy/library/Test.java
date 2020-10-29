3d0d710073511cc442d321d7ecdfc643e0c279a5
sunhuahui <sunhuahui@111.com.cn>
2020-10-29 14:48:50 +0800

    添加测试代码

---
+++ library/src/main/java/com/hy/library/Test.java
@@ -0,0 +1,11 @@
+package com.hy.library;
+
+import android.util.Log;
+
+public class Test {
+    private static final String TAG = "Test";
+
+    public static void test() {
+        Log.d(TAG, "test");
+    }
+}
